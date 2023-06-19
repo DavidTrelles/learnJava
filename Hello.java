1ae7de3d7d00c75b146a2d50acd5ee3433a0065c
David Trelles <davidstrelles@gmail.com>
2023-06-19 12:27:25 -0500

    learned about variables

--- Hello.java
+++ Hello.java
@@ -4,3 +4,15 @@ public class Review{
     System.out.print("My first Java program from scratch.");
   };
 }
+
+//you can Declare and assign variables in the same line - for example:
+public class Creator{
+    public static void main(String[]args)
+    {
+        String name = "James Gosling";
+        //we declared a name variable of string type and gave it a value of James gosling
+        //the creater of Java
+        System.out.println(name);
+        //that should print the variable to the console
+    }
+}
