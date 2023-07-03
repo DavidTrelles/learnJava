c1c17dca10ee9fdbe23f08bb688cd2ee462a990a
David Trelles <davidstrelles@gmail.com>
2023-07-03 11:31:39 -0500

    arithmetic operators and pemdas in java

--- Hello.java
+++ Hello.java
@@ -30,3 +30,21 @@ public class Booleans {
 //remember to use javac command - if you declare a variable incorrectly
 //it's important to use specific naming conventions, and camelCasing - 
 //variables names must start with $,_or letter, other names will not compile and are illegal
+public class Operations {
+	public static void main(String[] args) { 
+
+    int expression1 = 5 % 2 - (4 * 2 - 1);
+     System.out.println(expression1);
+//expected answer -6
+    int expression2 = (3 + (2 * 2 - 5)) + 6 - 5;
+     System.out.println(expression2);
+//3
+    int expression3 = 5 * 4 % 3 - 2 + 1;
+     System.out.println(expression3);
+//2
+	}
+}
+
+//at each level of pemdas you evaluate left to right
+// (for example Modulo/multiplication/division doesn't mean to do modulo first, 
+//you do whatever is on the left first)
